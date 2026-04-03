750563a86fb3fcdb8a57cff49d1674f7e64cb722
Gaurav <gaurav@acillp.com>
2026-04-03 17:15:53 +0530

    fix(cors): allow PATCH method for collections receivable updates

--- src/server.js
+++ src/server.js
@@ -41,7 +41,10 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", origin || "*");
   }
 
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+  );
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
